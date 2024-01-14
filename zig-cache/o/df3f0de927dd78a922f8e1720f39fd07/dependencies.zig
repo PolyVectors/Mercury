ce1e9bf34fbe8a9f7a5332a122e5f7b79957e0ff
PolyVector <PolyVector@protonmail.com>
2024-01-14 23:47:58 +0000

    Final (hopefully) fix for .gitignore

--- zig-cache/o/df3f0de927dd78a922f8e1720f39fd07/dependencies.zig
+++
@@ -1,6 +0,0 @@
-pub const imports = struct {
-    pub const clap = @import("1220f48518ce22882e102255ed3bcdb7aeeb4891f50b2cdd3bd74b5b2e24d3149ba2");
-};
-pub const build_root = struct {
-    pub const clap = "C:\\Users\\PolyV\\AppData\\Local\\zig\\p\\1220f48518ce22882e102255ed3bcdb7aeeb4891f50b2cdd3bd74b5b2e24d3149ba2";
-};
